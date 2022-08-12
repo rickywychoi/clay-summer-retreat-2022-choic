0616b00ebc68e4776cfa11ede444ded0ad439ee2
rickywychoi <cchaff0105@gmail.com>
2022-08-12 09:31:36 -0700

    Band-aid fix for react-image-gallery styles import

--- src/app/pages/Map/Map.tsx
+++ src/app/pages/Map/Map.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ImageGallery from 'react-image-gallery';
-// import 'react-image-gallery/styles/css/image-gallery.css';
+import '../../../styles/react-image-gallery/css/image-gallery.css'; // band-aid fix for deployment - otherwise need to figure out how craco picks up css files in node_modules!!
 import mapImage from '../../../assets/images/map.png';
 
 const images = [
@@ -13,7 +13,11 @@ const images = [
 ];
 
 const MapPage = () => {
-  return <ImageGallery items={images} showPlayButton={false} showIndex={true} showThumbnails={false} />;
+  return (
+    <div style={{ paddingBottom: '50px' }}>
+      <ImageGallery items={images} showPlayButton={false} showIndex={true} showThumbnails={false} />
+    </div>
+  );
 };
 
 export default MapPage;
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,4 @@ import ReactDOM from 'react-dom';
 import 'reflect-metadata';
 import App from './app/App';
 
-import 'react-image-gallery/styles/css/image-gallery.css';
-
 ReactDOM.render(<App />, document.getElementById('root'));
