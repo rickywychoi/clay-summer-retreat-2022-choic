2691fb91dd6b7218f27ce13dd016e5874bf86266
rickywychoi <cchaff0105@gmail.com>
2022-08-12 15:24:40 -0700

    Add proper questions for nodes

--- src/app/core/binaryTree/tree.ts
+++ src/app/core/binaryTree/tree.ts
@@ -27,28 +27,60 @@ const example = (): Node => {
 };
 
 const real = (): Node => {
-  // root
-  const root = new Node('root', { todo: '', place: 'place' });
-  // height 1
-  root.left = new Node('실내', { todo: '말씀 읽기를 하세요.', place: '본당' });
-  root.right = new Node('실외', { todo: '피구를 하세요.', place: 'Gym' });
-  // height 2
+  /**
+   * root
+   */
+  const root = new Node('root', { todo: '', place: 'place' }, '어디로 가시겠습니까?');
+  /**
+   * height 1
+   */
+  root.left = new Node('실내', { todo: '말씀 읽기를 하세요.', place: '본당' }, '다시 한번 어디로 가시겠습니까?');
+  root.right = new Node('실외', { todo: '피구를 하세요.', place: 'Gym' }, '무엇을 마시겠습니까?');
+  /**
+   * height 2
+   */
   root.left.left = new Node('실내', { todo: '말씀 읽기를 하세요.', place: '본당' });
   root.left.right = new Node('실외', { todo: '피구를 하세요.', place: 'Gym' });
   root.right.left = new Node('물', { todo: '하이킹을 떠나세요.', place: 'Thaletel Trail (Lexw Qwo:m Park)' });
   root.right.right = new Node('주스', { todo: '로프 코스를 하세요.', place: '로프 코스' });
-  // height 3
-  root.left.left.left = new Node('좌우', { todo: 'Archery를 하세요.', place: '로프 코스 옆' });
-  root.left.left.right = new Node('상하', { todo: '단체 줄넘기를 하세요.', place: 'Gym' });
-  root.left.right.left = new Node('혼자 있는 것', {
-    todo: '헤쳐있다가 고깔모자 쓰고 다시 모이기를 하세요.',
-    place: 'Gym'
-  });
-  root.left.right.right = new Node('그룹과 같이 있는 것', { todo: '로프 코스를 하세요.', place: '로프 코스' });
-  root.right.right.left = new Node('좌우', { todo: 'Archery를 하세요.', place: '로프 코스 옆' });
+  /**
+   * height 3
+   */
+  root.left.left.left = new Node(
+    '좌우',
+    { todo: 'Archery를 하세요.', place: '로프 코스 옆' },
+    '누구의 삶을 선택하시겠습니까?'
+  );
+  root.left.left.right = new Node(
+    '상하',
+    { todo: '단체 줄넘기를 하세요.', place: 'Gym' },
+    '어느 시대로 시간 여행을 가시겠습니까?'
+  );
+  root.left.right.left = new Node(
+    '혼자 있는 것',
+    {
+      todo: '헤쳐있다가 고깔모자 쓰고 다시 모이기를 하세요.',
+      place: 'Gym'
+    },
+    '누구의 삶을 선택하시겠습니까?'
+  );
+  root.left.right.right = new Node(
+    '그룹과 같이 있는 것',
+    { todo: '로프 코스를 하세요.', place: '로프 코스' },
+    '어느 시대로 시간 여행을 가시겠습니까?'
+  );
+  root.right.right.left = new Node(
+    '좌우',
+    { todo: 'Archery를 하세요.', place: '로프 코스 옆' },
+    '누구의 삶을 선택하시겠습니까?'
+  );
   root.right.right.right = new Node('상하', { todo: 'Climbing Tower로 가세요.', place: 'Climbing Tower' });
-  // 간식 타임
-  // height 4
+  /**
+   * 간식 타임
+   */
+  /**
+   * height 4
+   */
   root.left.left.left.left = new Node('다니엘', { todo: '단체 사진을 찍어오세요.', place: '야외' });
   root.left.left.left.right = new Node('바울', {
     todo: '놀이터로 가서 술래 한명을 정한다음, 술래가 고깔모자를 쓰고 놀이터에 흩어져있는 멤버들을 찾아오세요.',
@@ -57,23 +89,37 @@ const real = (): Node => {
   root.left.left.right.left = new Node('고구려 + 조선', { todo: 'Archery를 하세요.', place: '로프 코스 옆' });
   root.left.left.right.right = new Node('백제 + 신라', { todo: 'Climbing Tower로 가세요.', place: 'Climbing Tower' });
   root.left.right.left.left = new Node('다니엘', { todo: '단체 사진을 찍어오세요.', place: '야외' });
-  root.left.right.left.right = new Node('요나', {
-    todo: '수영장으로 가서 고깔모자를 쓰고 물총으로 수영장 위에 있는 물건들을 맞추기를 하세요.',
-    place: '수영장'
-  });
+  root.left.right.left.right = new Node(
+    '요나',
+    {
+      todo: '수영장으로 가서 고깔모자를 쓰고 물총으로 수영장 위에 있는 물건들을 맞추기를 하세요.',
+      place: '수영장'
+    },
+    '어떤 동물의 삶을 선택하시겠습니까?'
+  );
   root.left.right.right.left = new Node('고구려 + 조선', { todo: 'Archery를 하세요.', place: '로프 코스 옆' });
   root.left.right.right.right = new Node('백제 + 신라', { todo: 'Climbing Tower로 가세요.', place: 'Climbing Tower' });
-  root.right.right.left.left = new Node('요나', {
-    todo: '수영장으로 가서 고깔모자를 쓰고 물총으로 수영장 위에 있는 물건들을 맞추기를 하세요.',
-    place: '수영장'
-  });
+  root.right.right.left.left = new Node(
+    '요나',
+    {
+      todo: '수영장으로 가서 고깔모자를 쓰고 물총으로 수영장 위에 있는 물건들을 맞추기를 하세요.',
+      place: '수영장'
+    },
+    '어떤 동물의 삶을 선택하시겠습니까?'
+  );
   root.right.right.left.right = new Node('바울', {
     todo: '놀이터로 가서 술래 한명을 정한다음, 술래가 고깔모자를 쓰고 놀이터에 흩어져있는 멤버들을 찾아오세요.',
     place: '놀이터'
   });
   root.right.right.right.left = new Node('좌우', { todo: 'Archery를 하세요.', place: '로프 코스 옆' });
-  root.right.right.right.right = new Node('상하', { todo: '단체 줄넘기를 하세요.', place: 'Gym' });
-  // height 5
+  root.right.right.right.right = new Node(
+    '상하',
+    { todo: '단체 줄넘기를 하세요.', place: 'Gym' },
+    '누구의 삶을 선택하시겠습니까?'
+  );
+  /**
+   * height 5
+   */
   root.left.left.left.left.left = new Node('첫인상', {
     todo: '수련회 조 멤버들 첫인상으로 칭찬 써주기를 하세요.',
     place: '본당'
