b37e5e5421e1682b043250429bfba60e8fd6a34a
rickywychoi <cchaff0105@gmail.com>
2022-08-12 11:55:09 -0700

    Remove react-image-gallery but show raw img instead for map

--- src/app/pages/Map/Map.tsx
+++ src/app/pages/Map/Map.tsx
@@ -1,21 +1,10 @@
 import React from 'react';
-import ImageGallery from 'react-image-gallery';
-import './react-image-gallery/css/image-gallery.css'; // band-aid fix for deployment - otherwise need to figure out how craco picks up css files in node_modules!!
 import mapImage from '../../../assets/images/map.png';
-
-const images = [
-  {
-    original: mapImage,
-    thumbnail: mapImage,
-    loading: 'lazy',
-    thumbnailLoading: 'lazy'
-  }
-];
 
 const MapPage = () => {
   return (
     <div style={{ paddingBottom: '50px' }}>
-      <ImageGallery items={images} showPlayButton={false} showIndex={true} showThumbnails={false} />
+      <img src={mapImage} alt="charis camp map" style={{ maxWidth: '100%' }} />
     </div>
   );
 };
