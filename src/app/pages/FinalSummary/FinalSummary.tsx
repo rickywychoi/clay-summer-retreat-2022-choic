4b5380bb97da683dabbf23577b449b3164bd021e
rickywychoi <cchaff0105@gmail.com>
2022-08-13 21:31:26 -0700

    Add choice tree img in the final summary page

--- src/app/pages/FinalSummary/FinalSummary.tsx
+++ src/app/pages/FinalSummary/FinalSummary.tsx
@@ -1,17 +1,22 @@
-import React from 'react';
-import { inOrder } from '~/app/core/binaryTree/utils';
-import { tree } from '~/app/core/binaryTree/tree';
+import React, { useMemo } from 'react';
+import { checkIfLastChild, inOrder } from '~/app/core/binaryTree/utils';
+import { startDate, tree } from '~/app/core/binaryTree/tree';
 import { useLocalStorage } from '~/app/shared/localStorage/localStorage.hook';
 import SummaryCard from '~/app/components/SummaryCard';
 import Separator from '~/app/components/Separator';
+import treeImg from '~/assets/images/choice-tree.png';
 
 const root = tree();
 
 const FinalSummary = () => {
   const { choices } = useLocalStorage();
+  const isEnd = useMemo(() => {
+    const lastItem = inOrder(root, choices.at(-1));
+    return checkIfLastChild(lastItem);
+  }, [choices]);
 
-  return (
-    <div>
+  return isEnd ? (
+    <>
       <h1>THE END</h1>
       {choices
         .map((c) => inOrder(root, c))
@@ -23,14 +28,24 @@ const FinalSummary = () => {
             </div>
           );
         })}
-      <Separator height="50px" />
+      <Separator height="30px" />
+      {new Date() < startDate && (
+        <>
+          <h2>Choice Tree</h2>
+          <img src={treeImg} alt="tree-img" style={{ maxWidth: '100%' }} />
+          <Separator height="30px" />
+        </>
+      )}
       <p style={{ fontSize: '15px' }}>
         Creds to:{' '}
         <a href="https://github.com/rickywychoi" target="_blank" rel="noopener noreferrer">
           RC
         </a>
       </p>
-    </div>
+      <Separator height="20px" />
+    </>
+  ) : (
+    <div>돌아가세요.</div>
   );
 };
 
